06c8ab9411e67e8e66c4b19039f5da94e653296d
Marcus Meyer <marcusmeyer90@hotmail.fr>
2025-04-15 14:58:00 +0200

    Lists routes, controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,29 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-print "Cleaning database"
+puts "Cleaning database"
+Film.destroy_all
+puts "Database cleaned"
 #
 #
-print "Movie 1"
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+puts "OK"
 #
-print "Movie 2"
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 #
-print "Movie 3"
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+print "Film 1..."
+Film.create(titre: "Wonder Woman 1984", apercu: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", url_affiche: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", note: 6.9)
+puts "OK"
 #
-print "Movie 4"
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
-print "#{Movie.count} generated"
+print "Film 2..."
+Film.create(titre: "The Shawshank Redemption", apercu: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", url_affiche: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", note: 8.7)
+puts "OK"
+#
+print "Film 3..."
+Film.create(titre: "Titanic", apercu: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", url_affiche: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", note: 7.9)
+puts "OK"
+#
+print "Film 4..."
+Film.create(titre: "Ocean's Eight", apercu: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", url_affiche: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", note: 7.0)
+puts "OK"
+
+print "#{Film.count} films generated successfully"
